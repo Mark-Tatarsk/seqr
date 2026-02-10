361f9c7582b8abddb01bc645a83053470b92ae79
Mark-Tatarsk <mark.tatarskiy@gmail.com>
2026-02-10 17:00:46 +0100

    Fix RTI build: expand mocking to include sub-packages

--- docs/conf.py
+++ docs/conf.py
@@ -6,31 +6,52 @@ from unittest.mock import MagicMock
 sys.path.insert(0, os.path.abspath(".."))
 
 # -- Mocking modules for Read the Docs ---------------------------------------
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+def mock_package(name):
+    m = Mock()
+    m.__path__ = []
+    sys.modules[name] = m
+    return m
+
+# Mocking the package tree to avoid "is not a package" errors
+packages = [
+    "django", "django.conf", "django.db", "django.db.models", "django.core", 
+    "django.core.wsgi", "django.core.management", "django.contrib", "django.contrib.auth",
+    "google", "google.auth", "google.auth.transport",
+    "elasticsearch", "elasticsearch_dsl", "hail"
+]
+
+for pkg in packages:
+    mock_package(pkg)
+
 MOCK_MODULES = [
-    "django",
-    "django.conf",
     "django.conf.settings",
-    "django.db",
-    "django.db.models",
-    "django.contrib",
-    "django.contrib.auth",
+    "django.db.models.query",
+    "django.db.models.expressions",
     "django.contrib.auth.models",
     "psycopg2",
-    "hail",
-    "elasticsearch",
-    "elasticsearch_dsl",
-    "google",
-    "google.auth",
+    "google.auth.transport.requests",
     "social_django",
     "social_core",
     "anymail",
     "django-anymail",
     "clickhouse_driver",
     "redis",
+    "corsheaders",
+    "hijack",
 ]
 
 for mod in MOCK_MODULES:
     sys.modules[mod] = MagicMock()
+
+# Mock Django version to avoid comparison errors
+import django
+django.VERSION = (4, 2, 0, 'final', 0)
+django.get_version = lambda: "4.2"
 
 # -- Project information -----------------------------------------------------
 project = 'seqr'
@@ -42,7 +63,10 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
 ]
+
+autosummary_generate = True
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -52,7 +76,7 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # -- Autodoc configuration ---------------------------------------------------
-autodoc_mock_imports = MOCK_MODULES
+autodoc_mock_imports = MOCK_MODULES + packages
 
 # -- Napoleon settings -------------------------------------------------------
 napoleon_google_docstring = True
